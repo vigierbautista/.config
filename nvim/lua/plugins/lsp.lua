defe114f525fe28b51f624b0db8c7b62a9324408
Bautista Vigier <bautista.vigier@boehringer-ingelheim.com>
2025-07-10 17:19:43 +0000

    Added cmp configuration

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,27 +1,145 @@
-return {
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            {
-                "folke/lazydev.nvim",
-                ft = "lua", -- only load on lua files
-                opts = {
-                    library = {
-                        -- See the configuration section for more details
-                        -- Load luvit types when the `vim.uv` word is found
-                        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-                    },
-                },
-            },
+local root_files = {
+  '.luarc.json',
+  '.luarc.jsonc',
+  '.luacheckrc',
+  '.stylua.toml',
+  'stylua.toml',
+  'selene.toml',
+  'selene.yml',
+  '.git',
+}
 
-        },
-        config = function()
-            require("lspconfig").lua_ls.setup {}
-            require("lspconfig").pyright.setup {}
-        end
+return {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "stevearc/conform.nvim",
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/nvim-cmp",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+        "j-hui/fidget.nvim",
     },
-    {
-        "mason-org/mason.nvim",
-        opts = {}
-    },
+
+    config = function()
+        require("conform").setup({
+            formatters_by_ft = {
+            }
+        })
+        local cmp = require('cmp')
+        local cmp_lsp = require("cmp_nvim_lsp")
+        local capabilities = vim.tbl_deep_extend(
+            "force",
+            {},
+            vim.lsp.protocol.make_client_capabilities(),
+            cmp_lsp.default_capabilities())
+
+        require("fidget").setup({})
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "lua_ls",
+                "pyright",
+                "gopls",
+            },
+            handlers = {
+                function(server_name) -- default handler (optional)
+                    require("lspconfig")[server_name].setup {
+                        capabilities = capabilities
+                    }
+                end,
+
+                -- Prime's zig configuration
+                -- zls = function()
+                --     local lspconfig = require("lspconfig")
+                --     lspconfig.zls.setup({
+                --         root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
+                --         settings = {
+                --             zls = {
+                --                 enable_inlay_hints = true,
+                --                 enable_snippets = true,
+                --                 warn_style = true,
+                --             },
+                --         },
+                --     })
+                --     vim.g.zig_fmt_parse_errors = 0
+                --     vim.g.zig_fmt_autosave = 0
+                --
+                -- end,
+                ["lua_ls"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.lua_ls.setup {
+                        capabilities = capabilities,
+                        settings = {
+                            Lua = {
+                                format = {
+                                    enable = true,
+                                    -- Put format options here
+                                    -- NOTE: the value should be STRING!!
+                                    defaultConfig = {
+                                        indent_style = "space",
+                                        indent_size = "2",
+                                    }
+                                },
+                            }
+                        }
+                    }
+                end,
+            }
+        })
+
+        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+        local luasnip = require('luasnip')
+        
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
+
+                -- *** This is the core change for <Tab> behavior ***
+                ['<Tab>'] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        -- If completion menu is visible, confirm the selected item
+                        cmp.confirm({ select = true })
+                    elseif luasnip.expand_or_jumpable() then
+                        -- If a snippet can be expanded or jumped to, do that
+                        luasnip.expand_or_jump()
+                    else
+                        -- Otherwise, fall back to the default tab behavior (insert a literal tab)
+                        fallback()
+                    end
+                end, { 'i', 's' }), -- 'i' for insert mode, 's' for select mode
+            }),
+            sources = cmp.config.sources({
+                { name = "copilot", group_index = 2 },
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' }, -- For luasnip users.
+            }, {
+                { name = 'buffer' },
+            })
+        })
+
+        vim.diagnostic.config({
+            -- update_in_insert = true,
+            float = {
+                focusable = false,
+                style = "minimal",
+                border = "rounded",
+                source = "always",
+                header = "",
+                prefix = "",
+            },
+        })
+    end
 }
